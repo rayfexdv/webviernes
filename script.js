7bdfcecb625a0792f057514114b58171948e8742
rayfexdv <facundo.iromero@davinci.edu.ar>
2025-10-17 20:05:21 -0300

    Carga de proyecto

---
+++ script.js
@@ -0,0 +1,8 @@
+function cambiaColores(color, altura){
+    caja.style.backgroundColor = color;
+    caja.style.height = altura;
+}
+function reset(){ // estoy buscando article id="caja"
+    caja.style.backgroundColor = 'white';
+    caja.style.height = "auto";
+}
